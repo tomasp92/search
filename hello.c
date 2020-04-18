6dfc0a4e4a5eb4683f57eb86291075bc781a09ab
tomasp92 <tomasp92@users.noreply.github.com>
2020-04-18 05:00:47 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- hello.c
+++ hello.c
@@ -4,6 +4,8 @@
 
 int main(void)
 {
+    // ask the user its name and store the answer (answer) on te left
     string answer = get_string("whats your name?\n");
+    // say hello and the users name (answer)
     printf("hello,  %s\n", answer);
 }
