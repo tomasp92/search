b3ba4710990fbf1007b737564f92110321bd16c4
tomasp92 <tomasp92@users.noreply.github.com>
2020-05-17 15:47:04 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -20,20 +20,20 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
     RGBTRIPLE copyimage [height][width];
-    
+
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
             copyimage [i][j] = image [i][j];
-            
+
         }
     }
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            
+
             if(round (copyimage [i][j].rgbtRed * 0.393 + copyimage [i][j].rgbtGreen * 0.769 + copyimage [i][j].rgbtBlue * 0.189) > 254)
             {
                 image [i][j].rgbtRed = 255;
@@ -42,7 +42,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             {
                 image [i][j].rgbtRed = round (copyimage [i][j].rgbtRed * 0.393 + copyimage [i][j].rgbtGreen * 0.769 + copyimage [i][j].rgbtBlue * 0.189);
             }
-            
+
             if(round (copyimage [i][j].rgbtRed * 0.349 + copyimage [i][j].rgbtGreen * 0.686 + copyimage [i][j].rgbtBlue * 0.168) > 254)
             {
                 image [i][j].rgbtGreen =  255;
@@ -51,7 +51,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             {
                 image [i][j].rgbtGreen =  round (copyimage [i][j].rgbtRed * 0.349 + copyimage [i][j].rgbtGreen * 0.686 + copyimage [i][j].rgbtBlue * 0.168);
             }
-            
+
              if(round (copyimage [i][j].rgbtRed * 0.272 + copyimage [i][j].rgbtGreen * 0.534 + copyimage [i][j].rgbtBlue * 0.131) > 254)
             {
                 image [i][j].rgbtGreen =  255;
@@ -60,8 +60,8 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             {
                  image [i][j].rgbtBlue =  round (copyimage [i][j].rgbtRed * 0.272 + copyimage [i][j].rgbtGreen * 0.534 + copyimage [i][j].rgbtBlue * 0.131);
             }
-            
-           
+
+
         }
     }
     return;
@@ -86,25 +86,25 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
     RGBTRIPLE copyimage [height][width];
-    
+
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
             copyimage [i][j] = image [i][j];
-            
+
         }
     }
-    
+
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
             if( i == 0 && j == 0)
             {
-                image [i][j].rgbtRed =  round((float)copyimage [i][j].rgbtRed + (float) copyimage [i+1][j].rgbtRed +  copyimage [i][j+1].rgbtRed +  copyimage [i+1][j+1].rgbtRed)/4;
-                image [i][j].rgbtGreen =  round((float)copyimage [i][j].rgbtGreen + (float) copyimage [i+1][j].rgbtGreen +  copyimage [i][j+1].rgbtGreen +  copyimage [i+1][j+1].rgbtGreen)/4;
-                image [i][j].rgbtBlue =  round((float)copyimage [i][j].rgbtBlue + (float) copyimage [i+1][j].rgbtBlue +  copyimage [i][j+1].rgbtBlue +  copyimage [i+1][j+1].rgbtBlue)/4;
+                image [i][j].rgbtRed =  round((float)copyimage [i][j].rgbtRed + (float) copyimage [i+1][j].rgbtRed +  copyimage [i][j+1].rgbtRed + copyimage [i+1][j+1].rgbtRed)/4;
+                image [i][j].rgbtGreen =  round((float)copyimage [i][j].rgbtGreen + (float) copyimage [i+1][j].rgbtGreen +  copyimage [i][j+1].rgbtGreen + copyimage [i+1][j+1].rgbtGreen)/4;
+                image [i][j].rgbtBlue =  round((float)copyimage [i][j].rgbtBlue + (float) copyimage [i+1][j].rgbtBlue +  copyimage [i][j+1].rgbtBlue + copyimage [i+1][j+1].rgbtBlue)/4;
             }
             else if (i == 0 && j == width - 1)
             {
@@ -139,14 +139,14 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             else if(i == height - 1)
             {
                 image [i][j].rgbtRed =  round((float)copyimage [i][j].rgbtRed + (float) copyimage [i-1][j-1].rgbtRed + (float) copyimage [i-1][j].rgbtRed + (float) copyimage [i-1][j+1].rgbtRed + (float) copyimage [i][j-1].rgbtRed + (float) copyimage [i][j+1].rgbtRed)/6;
-                image [i][j].rgbtGreen =  round((float)copyimage [i][j].rgbtGreen + (float) copyimage [i-1][j-1].rgbtGreen + (float) copyimage [i-1][j].rgbtGreen + (float) copyimage [i-1][j+1].rgbtGreen + (float) copyimage [i][j-1].rgbtGreen)/6;
+                image [i][j].rgbtGreen =  round((float)copyimage [i][j].rgbtGreen + (float) copyimage [i-1][j-1].rgbtGreen + (float) copyimage [i-1][j].rgbtGreen + (float) copyimage [i-1][j+1].rgbtGreen + (float) copyimage [i][j-1].rgbtGreen + (float) copyimage [i][j+1].rgbtGreen)/6;
                 image [i][j].rgbtBlue =  round((float)copyimage [i][j].rgbtBlue + (float) copyimage [i-1][j-1].rgbtBlue + (float) copyimage [i-1][j].rgbtBlue + (float) copyimage [i-1][j+1].rgbtBlue + (float) copyimage [i][j-1].rgbtBlue + (float) copyimage [i][j+1].rgbtBlue)/6;
             }
             else if(j == width - 1)
             {
-                image [i][j].rgbtRed =  round((float)copyimage [i][j].rgbtRed + (float) copyimage [i+1][j].rgbtRed + (float) copyimage [i-1][j].rgbtRed + (float) copyimage [i+1][j-1].rgbtRed + (float) copyimage [i-1][j-1].rgbtRed + (float) copyimage [i+1][j].rgbtRed)/6;
-                image [i][j].rgbtGreen =  round((float)copyimage [i][j].rgbtGreen + (float) copyimage [i+1][j].rgbtGreen + (float) copyimage [i-1][j].rgbtGreen + (float) copyimage [i+1][j-1].rgbtGreen + (float) copyimage [i-1][j-1].rgbtGreen + (float) copyimage [i+1][j].rgbtGreen)/6;
-                image [i][j].rgbtBlue =  round((float)copyimage [i][j].rgbtBlue + (float) copyimage [i+1][j].rgbtBlue + (float) copyimage [i-1][j].rgbtBlue + (float) copyimage [i+1][j-1].rgbtBlue + (float) copyimage [i-1][j-1].rgbtBlue + (float) copyimage [i+1][j].rgbtBlue)/6;
+                image [i][j].rgbtRed =  round((float)copyimage [i][j].rgbtRed + (float) copyimage [i+1][j].rgbtRed + (float) copyimage [i-1][j].rgbtRed + (float) copyimage [i+1][j+1].rgbtRed + (float) copyimage [i-1][i+1].rgbtRed + (float) copyimage [i][j+1].rgbtRed)/6;
+                image [i][j].rgbtGreen =  round ((float)copyimage [i][j].rgbtGreen + (float) copyimage [i+1][j].rgbtGreen + (float) copyimage [i-1][j].rgbtGreen + (float) copyimage [i+1][j+1].rgbtGreen + (float) copyimage [i-1][j+1].rgbtGreen + (float) copyimage [i][j+1].rgbtGreen)/6;
+                image [i][j].rgbtBlue =  round((float)copyimage [i][j].rgbtBlue + (float) copyimage [i+1][j].rgbtBlue + (float) copyimage [i-1][j].rgbtBlue + (float) copyimage [i+1][j+1].rgbtBlue + (float) copyimage [i-1][j+1].rgbtBlue + (float) copyimage [i][j+1].rgbtBlue)/6;
             }
             else
             {
