16ef4d7a60e1a4643449abe39136725a9edf6e76
tomasp92 <tomasp92@users.noreply.github.com>
2020-05-19 23:19:19 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -11,10 +11,10 @@ BYTE chunks[512];
 // set a count for JPG Files
 int JPG_count;
 
-bool read_file (FILE *file);
-bool check_JPG_header (void);
-void create_new_JPG_file (void);
-void apend_to_existing_JPG_file (void);
+bool read_file(FILE *file);
+bool check_JPG_header(void);
+void create_new_JPG_file(void);
+void apend_to_existing_JPG_file(void);
 
 
 int main(int argc, char *argv[])
@@ -45,47 +45,47 @@ int main(int argc, char *argv[])
         read_file(card_raw);
     }
     while (read_file(card_raw));
-    
+
     fclose(card_raw);
     return 0;
 }
 
-bool read_file (FILE *file)
+bool read_file(FILE *file)
 {
     // Read chunks of 512
-    if (fread(chunks , sizeof(BYTE), 512, file)==512)
+    if (fread(chunks, sizeof(BYTE), 512, file) == 512)
     {
         // If its start of nwe JPG
-        if(check_JPG_header())
+        if (check_JPG_header())
         {
             JPG_count ++;
             // If its the first .JPG start writting the file
             if (JPG_count == 1)
             {
-                create_new_JPG_file ();
+                create_new_JPG_file();
             }
             // If not first JPG
             else
             {
                 // Close the file that we were writting to and open new file
-               create_new_JPG_file ();
+                create_new_JPG_file();
             }
 
         }
 
         // If i was writting to a file already keep writting
-        else if(JPG_count > 0)
+        else if (JPG_count > 0)
         {
-            apend_to_existing_JPG_file ();
+            apend_to_existing_JPG_file();
         }
 
-    return true;
+        return true;
     }
     return false;
 }
 
 
-bool check_JPG_header (void)
+bool check_JPG_header(void)
 {
     if (chunks[0] == 0xff && chunks[1] == 0xd8 && chunks[2] == 0xff && (chunks[3] & 0xf0) == 0xe0)
     {
@@ -96,28 +96,28 @@ bool check_JPG_header (void)
 
 void create_new_JPG_file (void)
 {
-                char *filename = malloc(sizeof(int)*3);
-                sprintf (filename,"%03i.jpg", JPG_count-1);
-                FILE *img = fopen(filename,"w");
-                
-                if (img == NULL)
-                {
-                    printf("Could not open. File not found\n");
-        
-                }
-                
-                fwrite(chunks, sizeof(BYTE),512, img);
-                fclose(img);
-                free(filename);
+    char *filename = malloc(sizeof(int) * 3);
+    sprintf(filename, "%03i.jpg", JPG_count - 1);
+    FILE *img = fopen(filename, "w");
+
+    if (img == NULL)
+    {
+        printf("Could not open. File not found\n");
+
+    }
+
+    fwrite(chunks, sizeof(BYTE), 512, img);
+    fclose(img);
+    free(filename);
     return;
 }
 
-void apend_to_existing_JPG_file (void)
+void apend_to_existing_JPG_file(void)
 {
-    char *filename = malloc(sizeof(int)*3);
-    sprintf (filename,"%03i.jpg", JPG_count-1);
-    FILE *img = fopen(filename,"a");
+    char *filename = malloc(sizeof(int) * 3);
+    sprintf(filename, "%03i.jpg", JPG_count - 1);
+    FILE *img = fopen(filename, "a");
     free(filename);
-    fwrite(chunks, sizeof(BYTE),512, img);
+    fwrite(chunks, sizeof(BYTE), 512, img);
     fclose(img);
 }
