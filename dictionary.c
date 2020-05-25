2e4b61e75b4afdf6829433cb41e4989867fb81c6
tomasp92 <tomasp92@users.noreply.github.com>
2020-05-25 02:32:51 +0000

    automated commit by check50 [check50=True]

--- dictionary.c
+++ dictionary.c
@@ -114,9 +114,7 @@ bool load(const char *dictionary)
 // Returns number of words in dictionary if loaded else 0 if not yet loaded
 unsigned int size(void)
 {
-    // TODO
     return words_num;
-    printf("!!!NUMBER WORDS DICT: %i",words_num);
 }
 
 // Unloads dictionary from memory, returning true if successful else false
